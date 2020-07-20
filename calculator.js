8bf199ea15ad2a4af200abafc5e74f7f3753da9d
Hexiang Zhang <sam10.zhang@gmail.com>
2020-07-20 15:34:38 -0500

    setup styling, added buttons with createButton method

---
+++ calculator.js
@@ -0,0 +1,121 @@
+let calculator_buttons = [{
+    name: "delete",
+    symbol: "⌫",
+    formula: false,
+    type: "key"
+}, {
+    name: "clear",
+    symbol: "C",
+    formula: false,
+    type: "key"
+}, {
+    name: "percent",
+    symbol: "%",
+    formula: "/100",
+    type: "number"
+}, {
+    name: "division",
+    symbol: "÷",
+    formula: "/",
+    type: "operator"
+}, {
+    name: "7",
+    symbol: 7,
+    formula: 7,
+    type: "number"
+}, {
+    name: "8",
+    symbol: 8,
+    formula: 8,
+    type: "number"
+}, {
+    name: "9",
+    symbol: 9,
+    formula: 9,
+    type: "number"
+}, {
+    name: "multiplication",
+    symbol: "×",
+    formula: "*",
+    type: "operator"
+}, {
+    name: "4",
+    symbol: 4,
+    formula: 4,
+    type: "number"
+}, {
+    name: "5",
+    symbol: 5,
+    formula: 5,
+    type: "number"
+}, {
+    name: "6",
+    symbol: 6,
+    formula: 6,
+    type: "number"
+}, {
+    name: "addition",
+    symbol: "+",
+    formula: "+",
+    type: "operator"
+}, , {
+    name: "1",
+    symbol: 1,
+    formula: 1,
+    type: "number"
+}, {
+    name: "2",
+    symbol: 2,
+    formula: 2,
+    type: "number"
+}, {
+    name: "3",
+    symbol: 3,
+    formula: 3,
+    type: "number"
+}, {
+    name: "subtraction",
+    symbol: "–",
+    formula: "-",
+    type: "operator"
+}, {
+    name: "0",
+    symbol: 0,
+    formula: 0,
+    type: "number"
+}, {
+    name: "comma",
+    symbol: ".",
+    formula: ".",
+    type: "number"
+}, {
+    name: "calculate",
+    symbol: "=",
+    formula: "=",
+    type: "calculate"
+}];
+
+// SELECT ELEMENTS
+const input_element = document.querySelector(".input");
+const output_result_element = document.querySelector(".result .value");
+const output_operation_element = document.querySelector(".operation .value");
+
+// CREATE CALC BUTTONS
+function createButtons() {
+    const btns_per_row = 4;
+    let added_btns = 0;
+
+    calculator_buttons.forEach(button => {
+        if (added_btns % btns_per_row == 0) {
+            input_element.innerHTML += `<div class="row"></div>`;
+        }
+
+        const row = document.querySelector(".row:last-child");
+
+        row.innerHTML += `<button id="${button.name}">${button.symbol}</button>`;
+
+        added_btns++;
+    })
+}
+
+createButtons();
